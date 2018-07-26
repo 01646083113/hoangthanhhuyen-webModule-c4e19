5db550bf0948601c9f4863f8a1f0ff465f0275fa
Huyen Hoang <huyenpt1001@gmail.com>
2018-07-26 19:19:16 +0700

    initial commit

--- web01/homeword/user/user.py
+++ web01/homeword/user/user.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Danh sách lớp chưa hoàn thiện !! "
+    return """
+
+     Mưa, mình chưa từng ghét mà ngược lại . 
+    Nhưng dạo này nó cứ mang đến nỗi sợ cho mình mỗi đêm ,
+    nó làm mình mất ngủ, nó làm mình suy nghĩ ... 
+    Tiên sư cha chúng nó, mất điện ngủ éo làm được bài tập ! :-@
+             """
 
 @app.route('/user/<username>')
 def get_info_user(username):
@@ -12,62 +18,81 @@ def get_info_user(username):
     
         'quan': {
             'name': 'Nguyễn Anh Quân',
-            'age': 16
+            'age': 16,
+            'gender': 'Nữ'
             },
 
         'tuananh': {
             'name': 'Huỳnh Tuấn Anh',
-            'age': 23
+            'age': 23,
+            'gender': 'Nữ'
             },
 
         'huyen': {
             'name': 'Hoàng Thanh Huyền',
-            'age': 15
+            'age': 15,
+            'gender': 'Nữ'
             },
 
         'lien': {
             'name': 'Phẫn Kim Liên ',
-            'age': 18
+            'age': 18,
+            'gender': 'Nữ'
             },
 
         'ngocanh': {
             'name': 'Nguyễn Ngọc Anh',
-            'age': 23
+            'age': 23,
+            'gender': 'Nữ'
             },
 
         'nhung': {
             'name': 'Nguyễn Hồng Nhung',
-            'age': 21
+            'age': 21,
+            'gender': 'Nữ'
+
             },
 
         'linh': {
             'name': 'Việt Linh',
-            'age': 19
+            'age': 19,
+            'gender': 'Nam'
+
             },
 
         'ha': {
             'name': 'Anh Hà',
-            'age': 21
+            'age': 21,
+            'gender': 'Nam'
+
             },
 
         'thu': {
             'name': 'Bạn Thu',
-            'age': 18
+            'age': 18,
+            'gender': 'Nữ'
+
             },
 
         'dieu': {
             'name': 'Bạn Diệu',
-            'age': 21
+            'age': 21,
+            'gender': 'Nữ'
+
             },
 
         'phuong': {
             'name': 'Chị Phương',
-            'age': 21
+            'age': 21,
+            'gender': 'Nữ'
+
             },
 
         'hoang': {
             'name': 'Anh Hoàng',
-            'age': 21
+            'age': 21,
+            'gender': 'Nam'
+
             }
         }
     if username not in users:
