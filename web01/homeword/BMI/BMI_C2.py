e0166ec2d3962f5b56ddce2681a9893717563fab
Huyen Hoang <huyenpt1001@gmail.com>
2018-07-26 20:39:50 +0700

    initial commit

--- web01/homeword/BMI/BMI_C2.py
+++ web01/homeword/BMI/BMI_C2.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return ('em lại chào a Quý :v nếu web không chạy thì a vào atom bỏ cmt nhá :)))')
+    return ("")
 
 @app.route('/bmi/<int:weight>/<int:height>')
 def calc(weight, height):
